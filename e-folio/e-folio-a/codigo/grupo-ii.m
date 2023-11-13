c2d0b002ae46be9142490f5002d050e427eb04ab
cpmachado <cpmachado@protonmail.com>
2023-11-13 15:18:56 +0000

    Grupo II completo

--- e-folio/e-folio-a/codigo/grupo-ii.m
+++ e-folio/e-folio-a/codigo/grupo-ii.m
@@ -1,3 +1,7 @@
+pkg load symbolic
+
+syms r3
+
 A = [
 	2, 1, 0, 3, 0;
 	3, -1, 1, 4, 0;
@@ -6,12 +10,13 @@ A = [
 	0, 0, 1, 0, -2
 ];
 
-r_3 = 909;
+b = [0; 1; 2; r3; 0];
 
-b = [0; 1; 2; r_3; 0];
+x4(r3) = (A\b)(4, 1);
 
 % alínea a
 printf("det(A): %d\n", det(A));
 
 % alínea b
-printf("x_4: %e\n", (A\b)(4, 1));
+printf("x4: %s, %.2f\n", char(x4(909)), eval(x4(909)));
+
